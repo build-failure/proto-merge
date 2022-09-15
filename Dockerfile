21ceebb985db3a5d6b28be5c5a370d9c9cf51329
mlewk <mlewk@amazon.de>
2022-09-15 13:07:05 +0200

    Polish first working version

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,14 @@ COPY proto-merge.sh /proto-merge.sh
 
 RUN chmod +x /proto-merge.sh /docker-entrypoint.sh
 
+ENV PROTO_PATH_OUT="/tmp/"
+
+ENV PROTO_PATH_IN="/"
+
 ENV EXCLUDE_PATTERN="import\s*|package\s*|syntax\s*="
+
+ENV PRINT_STDOUT=true
+
+ENV DEBUG=false
 
 ENTRYPOINT ["/docker-entrypoint.sh"]
--- proto-merge.sh
+++ proto-merge.sh
@@ -4,30 +4,49 @@ function proto_merge() {
     local proto_file="$1"
     local proto_file_name=$(basename "$proto_file")
     local proto_file_out="$PROTO_PATH_OUT/$proto_file_name"
+    local redirect_std_err
 
-    protoc --descriptor_set_out=/merged_schema.pb \
-    --proto_path="$PROTO_PATH_IN" \
-    --dependency_out=/dev/stdout \
-    --include_imports "$proto_file" \
-    2>/dev/null \
-        | awk "1" RS=".\n" \
-        | xargs cat \
-        | grep -vwE "$EXCLUDE_PATTERN" > "$proto_file_out"
+    if [ "$DEBUG" = "false" ]; then
+        protoc --descriptor_set_out=/merged_schema.pb \
+            --proto_path="$PROTO_PATH_IN" \
+            --dependency_out=/dev/stdout \
+            --include_imports "$proto_file" \
+            2>/dev/null \
+                | awk "1" RS=".\n" \
+                | xargs cat \
+                | grep -vwE "$EXCLUDE_PATTERN" > "$proto_file_out"
+    else
+        protoc --descriptor_set_out=/merged_schema.pb \
+            --proto_path="$PROTO_PATH_IN" \
+            --dependency_out=/dev/stdout \
+            --include_imports "$proto_file" \
+                | awk "1" RS=".\n" \
+                | xargs cat \
+                | grep -vwE "$EXCLUDE_PATTERN" > "$proto_file_out"
+    fi
+
+    if [ "$PRINT_STDOUT" = "true" ]; then
+        cat "$proto_file_out"
+    fi
 }
 
 for i in "$@"; do
   case $i in
     -e=*|--exclude_pattern=*)
       EXCLUDE_PATTERN="${i#*=}"
-      shift # past argument=value
+      shift
       ;;
     -i=*|--proto_path_in=*)
       PROTO_PATH_IN="${i#*=}"
-      shift # past argument=value
+      shift
       ;;
     -o=*|--proto_path_out=*)
       PROTO_PATH_OUT="${i#*=}"
-      shift # past argument with no value
+      shift
+      ;;
+    -d|--print_stdout)
+      PRINT_STDOUT=true
+      shift
       ;;
     -*|--*)
       echo "Unknown option $i"
@@ -39,10 +58,6 @@ for i in "$@"; do
       ;;
   esac
 done
-
-echo "EXCLUDE_PATTERN $EXCLUDE_PATTERN"
-echo "PROTO_PATH_IN $PROTO_PATH_IN"
-echo "files: ${PROTO_FILES[*]}"
 
 for proto_file in "${PROTO_FILES[@]}"
 do
